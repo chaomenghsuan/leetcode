c0a8af93f474a0f690455e297311d5b83c08efc2
Mengxuan Zhao <chaomenghsuan@hotmail.com>
2021-07-07 21:24:57 -0400

    Create 528_RandomPickWithWeight.py

---
+++ 528_RandomPickWithWeight.py
@@ -0,0 +1,15 @@
+import itertools
+import random
+import bisect
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.w = list(itertools.accumulate(w))
+
+    def pickIndex(self) -> int:
+        return bisect.bisect_left(self.w, random.randint(1, self.w[-1]))
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
